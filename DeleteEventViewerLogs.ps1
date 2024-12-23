6629f0782d6d8edaa01481e99231b4a0ccdfaf97
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2024-12-23 14:01:02 +0200

    Update DeleteEventViewerLogs.ps1

--- DeleteEventViewerLogs.ps1
+++ DeleteEventViewerLogs.ps1
@@ -1,7 +1,7 @@
 
 
 # Define a function to clear security logs using wevtutil
-function Invoke-ClearSecurityLogs {
+function Invoke-ClearApplicationLogs {
 # Check if running as Administrator
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Output "This script must be run as an Administrator. Please restart PowerShell with elevated permissions."
@@ -9,8 +9,8 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
     try {
         # Clear the Security event logs using wevtutil
-        wevtutil cl Security
-        Write-Output "Security logs have been cleared using wevtutil."
+        wevtutil cl Application
+        Write-Output "Application logs have been cleared using wevtutil."
     } catch {
         Write-Output "Failed to clear Security logs. Ensure you have administrative privileges."
     }
